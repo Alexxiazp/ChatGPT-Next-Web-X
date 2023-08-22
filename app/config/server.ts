fcbdea65d5dccc90b3082ca565d3030ab5e54344
夏志鹏 <46433618+Alexxiazp@users.noreply.github.com>
2023-08-22 08:28:49 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -7,7 +7,6 @@ declare global {
       CODE?: string;
       BASE_URL?: string;
       PROXY_URL?: string;
-      VERCEL?: string;
       HIDE_USER_API_KEY?: string; // disable user's api key input
       DISABLE_GPT4?: string; // allow user to use gpt-4 or not
       BUILD_MODE?: "standalone" | "export";
@@ -44,7 +43,6 @@ export const getServerSideConfig = () => {
     needCode: ACCESS_CODES.size > 0,
     baseUrl: process.env.BASE_URL,
     proxyUrl: process.env.PROXY_URL,
-    isVercel: !!process.env.VERCEL,
     hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
     disableGPT4: !!process.env.DISABLE_GPT4,
     hideBalanceQuery: !!process.env.HIDE_BALANCE_QUERY,
